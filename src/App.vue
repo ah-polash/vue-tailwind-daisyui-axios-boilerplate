392f644f4abe9d91ac27392b608740a141bca2d3
Hayat <abuhayatpolash3@gmail.com>
2023-01-29 13:23:13 +0600

    add to git

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+</script>
+
+<template >
+  <div class="container mx-auto">
+    <h2 class="text-3xl bg-red-500 text-white">Hello Wordl!</h2>
+    <div className="btn-group">
+      <button className="btn btn-active">Button</button>
+      <button className="btn">Button</button>
+      <button className="btn">Button</button>
+    </div>
+  </div>
+</template>
+
+
+<style scoped>
+
+</style>
